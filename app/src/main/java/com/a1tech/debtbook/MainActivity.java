2d8a73d596e89e6a9b5e28c9a4f0affdcd1a6420
dckrOff <azamatotaboev7621@gmail.com>
2022-05-23 15:06:51 +0500

    project is created

---
+++ app/src/main/java/com/a1tech/debtbook/MainActivity.java
@@ -0,0 +1,24 @@
+package com.a1tech.debtbook;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+    private void init() {
+
+    }
+
+}

bd91e60bbb700efa315b463dc89e97d4f9fd8a78
dckrOff <azamatotaboev7621@gmail.com>
2022-05-25 17:27:26 +0500

    25.05.2022

--- app/src/main/java/com/a1tech/debtbook/MainActivity.java
+++ app/src/main/java/com/a1tech/debtbook/MainActivity.java
@@ -1,12 +1,16 @@
 package com.a1tech.debtbook;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MainActivity";
+    private ImageView ivMenu, ivSearch;
+    private RecyclerView rvClients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
-
+        ivMenu = findViewById(R.id.iv_menu);
+        ivSearch = findViewById(R.id.iv_search);
+        rvClients = findViewById(R.id.rv_clients);
     }
-
 }
